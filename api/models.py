277b2aab15d4c71a686bcea9dc2f58061c7b4395
Daniel Porzio <daniel.porzio@moove-it.com>
2019-05-28 20:01:50 -0300

    Add crimes 2005 data

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Crime(models.Model):
-    location = models.CharField(max_length=200)
-    primary_type = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
+    location     = models.CharField(max_length       =200)
+    primary_type = models.CharField(max_length       =200)
+    created_at   = models.DateTimeField(auto_now_add =True)
