ec0ca6465a3c6769a732c838d895fe3711da361d
Nicolas <nicofh@hotmail.com>
2019-07-19 19:51:35 -0300

    hdbscan

--- api/interactors/clustering.py
+++ api/interactors/clustering.py
@@ -2,9 +2,18 @@
 from sklearn.cluster import DBSCAN
 import pandas as pd
 from ..models import Crime
+import hdbscan
 import pdb
 
-class Clustering():
+class Clustering:
+    def dbscan(data):
+        clustering = DBSCAN(eps=0.015).fit(data)
+        return clustering.labels_
+
+    def hdbscan(data):
+        clustering = hdbscan.HDBSCAN().fit(data)
+        return clustering.labels_
+
     def clusterize(data, algorithm):
         variables = data[0].keys()
         df = pd.DataFrame(data, columns = variables)
@@ -12,16 +21,17 @@ class Clustering():
         df_ = df[include]
 
         if algorithm == 'DBSCAN':
-          clustering = DBSCAN(eps=0.015).fit(df_)
-          df_list = df_.values.tolist()
-          models = []
-          for i in range(len(df_)):
-            model = {'latitude': df_list[i][0], 'longitude': df_list[i][1], 'label': clustering.labels_[i]}
-            models.append(model)
-          return models
+          labels = Clustering.dbscan(df_)
         elif algorithm == 'DBSCAN++':
           return data
         elif algorithm == 'HDBSCAN':
-          return data
+          labels = Clustering.hdbscan(df_)
         else:
           return data
+
+        df_list = df_.values.tolist()
+        models = []
+        for i in range(len(df_)):
+          model = {'latitude': df_list[i][0], 'longitude': df_list[i][1], 'label': labels[i]}
+          models.append(model)
+        return models
--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from .models import Crime
+import pdb
 
 class CrimeSerializer(serializers.ModelSerializer):
     label=serializers.SerializerMethodField()
     def get_label(self, obj):
-        return obj['label']
+        label = 0
+        if isinstance(obj, dict):
+            label = obj['label']
+        return label
     class Meta:
         model = Crime
         fields=('latitude','longitude','label')
