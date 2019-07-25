ed698d8831886e79b7677ea638a5635db8885f07
Valentina Franchi <valef2894@gmail.com>
2019-07-24 23:18:02 -0300

    Fix filters for multiple options

--- api/views.py
+++ api/views.py
@@ -12,9 +12,22 @@ class CreateView(generics.ListCreateAPIView):
     model = Crime.objects
     params = self.request.GET.copy()
     algorithm = params.pop('algorithm', ['None'])[0]
+
+    filteredAtt = []
     for k,vals in params.lists():
+      tmp_model = None
       for v in vals:
-        model = model.filter(**{k: v})
+        if not tmp_model:
+          tmp_model = model.filter(**{k: v})
+        aux = model.filter(**{k: v})
+        newmodel = tmp_model.union(aux)
+      filteredAtt.append(newmodel)
+
+    if len(filteredAtt) > 0:
+      model = filteredAtt[0]
+      for f in filteredAtt:
+        model = model.intersection(f)
+        
     if algorithm == 'None':
         return model
     else:
