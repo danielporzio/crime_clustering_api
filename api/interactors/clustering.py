bb070693054bf2daa462baf135e2dc9b0b110df4
Nicolas <nicofh@hotmail.com>
2019-08-15 21:12:35 -0300

    min max kmeans

--- api/interactors/clustering.py
+++ api/interactors/clustering.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import hdbscan
 from sklearn.cluster import DBSCAN
-from sklearn.cluster import KMeans
+from . import kmeans
+from math import floor
+from numpy.random import randint
+import pdb
 
 def clusterize(data, algorithm, algorithm_params):
     df_ = generate_data_frame(data)
     sample_weights = get_weights(data, algorithm_params['use_weights'])
     labels = run_algorithm(df_, sample_weights, algorithm, algorithm_params)
-    return build_model(df_, labels, data)
+    return build_model(df_, labels, data, algorithm_params['use_weights'])
 
 def generate_data_frame(data):
     variables = data[0].keys()
@@ -27,10 +30,10 @@ def run_algorithm(data_frame, sample_weights, algorithm, algorithm_params):
     elif algorithm == 'HDBSCAN':
         labels = hdbscan_algorithm(data_frame, algorithm_params)
     else:
-        labels = kmeans(data_frame, sample_weights, algorithm_params)
+        labels = kmeans.run(data_frame, sample_weights, algorithm_params)
     return labels
 
-def build_model(data_frame, labels, data):
+def build_model(data_frame, labels, data, use_weights):
     df_list = data_frame.values.tolist()
     models = []
     for i in range(len(data_frame)):
@@ -38,7 +41,7 @@ def build_model(data_frame, labels, data):
             'latitude': df_list[i][0],
             'longitude': df_list[i][1],
             'label': labels[i],
-            'crime_weight': data[i]['crime_weight']
+            'crime_weight': data[i]['crime_weight'] if (use_weights  == 'True') else 1
         }
         models.append(model)
     return models
@@ -60,9 +63,3 @@ def hdbscan_algorithm(data_frame, params):
     if params['min_sample_size'] != 'None':
         min_samples = int(params['min_sample_size'])
     return hdbscan.HDBSCAN(min_cluster_size, min_samples).fit(data_frame).labels_
-
-def kmeans(data_frame, sample_weights, params):
-    n_clusters = 8
-    if params['n_clusters'] != 'None':
-        n_clusters = int(params['n_clusters'])
-    return KMeans(n_clusters).fit(data_frame, sample_weight=sample_weights).labels_
